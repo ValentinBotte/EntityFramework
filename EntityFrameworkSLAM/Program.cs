4e3231ba60ddc2e91c36d3e34adcf14cecb184a1
BOTTE Valentin <botte.v@sio.lan>
2016-10-03 11:32:18 +0200

    Fin scrit page 11

--- EntityFrameworkSLAM/Program.cs
+++ EntityFrameworkSLAM/Program.cs
@@ -11,6 +11,47 @@ namespace EntityFrameworkSLAM
         static void Main(string[] args)
         {
 
+            using (OracleEntities oracleContexte = new OracleEntities())
+            {
+
+                var requeteEmployes = from EMPLOYE in oracleContexte.EMPLOYEs
+                                      select EMPLOYE;
+
+                var lesEmployes = requeteEmployes.ToList();
+
+                foreach(var unEmploye in lesEmployes)
+                {
+                    Console.WriteLine(unEmploye.NUMEMP + " - " + unEmploye.NOMEMP);
+                }
+
+                Console.WriteLine("-----------------------------------");
+
+                var unCodeProjet = "PR1";
+                var requeteEmployesProjet = from EMPLOYE in oracleContexte.EMPLOYEs
+                                            where EMPLOYE.CODEPROJET.TrimEnd() == unCodeProjet
+                                            select EMPLOYE;
+
+                lesEmployes = requeteEmployesProjet.ToList();
+
+                foreach(var unEmploye in lesEmployes)
+                {
+                    Console.WriteLine(unEmploye.NUMEMP + " - " + unEmploye.NOMEMP);
+                }
+
+                Console.WriteLine("-----------------------------------");
+
+                var idEmploye = 3;
+                var requeteEmployesById = from EMPLOYE in oracleContexte.EMPLOYEs
+                                            where EMPLOYE.NUMEMP == idEmploye
+                                            select EMPLOYE;
+
+                var employeId = requeteEmployesById.First();
+                Console.Write(employeId.NOMEMP + " - " + employeId.PRENOMEMP + " - " + employeId.SALAIRE);
+            }
+
+           
+
+            Console.Read();
 
 
         }
